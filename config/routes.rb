326976a4ec36e17235da5c40b791d0a701282ab6
rogeruwayezu <rogeruwayezu@gmail.com>
2017-05-30 20:03:34 +0200

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  routing
+   get "/login" => "sessions#new"
+  post "/login" => "sessions#create"
+  get "/logout" => "sessions#destroy"
 end

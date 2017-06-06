ea5ec6febd7c8910437eeacfbab82339d6a1fe3c
Obedb <obedbigirimana@gmail.com>
2017-06-06 18:39:02 +0200

    Added password digest column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170601163349) do
+ActiveRecord::Schema.define(version: 20170606163126) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,9 +52,10 @@ ActiveRecord::Schema.define(version: 20170601163349) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
     t.integer  "role_id"
+    t.string   "password_digest"
   end
 
 end

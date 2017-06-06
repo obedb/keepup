c9e4c69faf521b165df8474ee5c4f74cd1d65d54
Karoli Kolokonyi <karoli@kepler.org>
2017-06-06 18:31:13 +0200

    Modified email domain

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,4 @@
 class UsersController < ApplicationController
-validates_format_of :email, with: /\@kepler\.com/, message: 'You should have an email with kepler.com'
+validates_format_of :email, with: /\@kepler\.org/, message: 'You should have an email with kepler.org'
 
 end

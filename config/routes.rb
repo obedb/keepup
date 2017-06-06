2ed68261bda61612944d701f6561fa71e009deb9
Obedb <obedbigirimana@gmail.com>
2017-06-06 18:25:42 +0200

    enabled restful actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
-
-   get "/login" => "sessions#new"
+  get "/home", to: 'posts#home'
+  get "/posts", to: 'posts#index'
+  get "/posts/new", to: 'posts#new'
+  get "/posts/:id", to: 'posts#show'
+  post "/posts", to: 'posts#create'
+  get "/posts/:id/edit", to: 'posts#edit'
+  patch "/posts/:id", to: 'posts#update'
+  delete "/posts/:id", to: 'posts#destroy'
+  get "/login" => "sessions#new"
   post "/login" => "sessions#create"
   get "/logout" => "sessions#destroy"
 end
+
+
+ 

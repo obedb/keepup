37a66bf9f3456123dd52c5c99d4f9b9eb407ff49
rogeruwayezu <rogeruwayezu@gmail.com>
2017-06-08 18:16:57 +0200

    changed column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170606163126) do
+ActiveRecord::Schema.define(version: 20170608161122) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,12 +50,13 @@ ActiveRecord::Schema.define(version: 20170606163126) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
+    t.string   "first_name"
     t.string   "email"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.integer  "role_id"
     t.string   "password_digest"
+    t.string   "last_name"
   end
 
 end

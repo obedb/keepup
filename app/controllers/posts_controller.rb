023fa9a229bc94f23a825ce4bfdbef77d3c964fa
Karoli Kolokonyi <karoli@kepler.org>
2017-06-13 20:00:55 +0200

    Added before action authentifications

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
 class PostsController < ApplicationController
+before_action :authenticate_admin!, only: [:new, :create, :edit, :update, :destroy]
+before_action :authenticate_staff!, only: [:new, :create, :edit, :update, :destroy]
+
   def home
     
   end

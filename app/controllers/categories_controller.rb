45bda8e455cf9d22917a21f705f479a974e8b6c4
Obedb <obedbigirimana@gmail.com>
2017-06-20 19:29:54 +0200

    Recreated categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,11 +1,8 @@
 class CategoriesController < ApplicationController
-<<<<<<< HEAD
+
   def index
     @categories = Category.all
     
   end
-=======
-  before_action :authenticate_admin!, only: [:new, :create, :edit, :update, :destroy]
-  before_action :authenticate_staff!, only: [:new, :create, :edit, :update, :destroy]
->>>>>>> f9219898052f76c5778529586a8b84b0a3abdcc4
+
 end

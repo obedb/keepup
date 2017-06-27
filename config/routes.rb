5cefdd7ecc4a2b1c6d6ac0585b9901ca363fdd11
Karoli Kolokonyi <karoli@kepler.org>
2017-06-27 19:20:46 +0200

    Resolved posts controller conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
 
-<<<<<<< HEAD
-  get"/home", to: 'posts#home'
   get "/index", to: 'posts#index'
 
-=======
-  get "/index", to: 'posts#index'
->>>>>>> d210f5d10b73d89baa6acffba58709053e7179ee
   get "/contacts", to: 'posts#contacts'
   get "/posts", to: 'posts#index'
   get "/posts/new", to: 'posts#new'
@@ -16,10 +11,6 @@ Rails.application.routes.draw do
   patch "/posts/:id", to: 'posts#update'
   delete "/posts/:id", to: 'posts#destroy'
 
-<<<<<<< HEAD
-
-=======
->>>>>>> d210f5d10b73d89baa6acffba58709053e7179ee
   get "/comments/new", to: 'comments#new'
   post "/comments", to: 'comments#create'
   delete "/comments/:id", to: 'comments#destroy'
@@ -28,8 +19,8 @@ Rails.application.routes.draw do
   get "/login" => "sessions#new"
   post "/login" => "sessions#create"
   get "/logout" => "sessions#destroy"
+  delete "/logout" => "sessions#destroy"
 
-  
 
   get "/signup" => "users#new" 
   post "/users" => "users#create"
